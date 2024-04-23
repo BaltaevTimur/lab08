3cd4d276698735bafe9f4675d27ea7988c08fbb7
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-04-23 19:50:54 +0300

    added actions.yml

--- .github/workflows/test1.sh
+++ .github/workflows/test1.sh
@@ -1,9 +1,9 @@
 root=/home/timur/BaltaevTimur/workspace/lab04
-cmake --build $root/formatter_lib
-cmake --build $root/formatter_ex_lib 
-cmake --build $root/hello_world_application 
-cmake --build $root/solver_lib
-cmake --build $root/solver_application
+cmake --build $root/formatter_lib/_buind
+cmake --build $root/formatter_ex_lib/_buind 
+cmake --build $root/hello_world_application/_buind 
+cmake --build $root/solver_lib/_buind
+cmake --build $root/solver_application/_buind
 
 $root/hello_world_application/_build/main
 echo -e '1\n2\n3' | $root/solver_application/_build/main
